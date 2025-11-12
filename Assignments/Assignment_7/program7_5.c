98d86a1806e13c538a8569233a335fa95850455e
TejasSutar9 <tejassutar9001@gmail.com>
2025-11-12 08:23:48 +0530

    Add Assignment_7 (Q1 - Q5)

---
+++ Assignments/Assignment_7/program7_5.c
@@ -0,0 +1,32 @@
+//5
+#include<stdio.h>
+
+void MultipleDisplay(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t",iNo*iCnt);
+    }
+    
+}
+
+//Time Complexity : O(N)
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    MultipleDisplay(iValue);
+
+    return 0;
+}
