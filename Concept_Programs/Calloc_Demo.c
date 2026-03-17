81d1e88db011951e0a4659c594ebf86516889b2a
Tejas Sutar <tejassutar9001@gmail.com>
2026-03-18 01:07:59 +0530

    Added C programming concept programs

---
+++ Concept_Programs/Calloc_Demo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int length=0;
+    int *Arr=NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d",&length);
+
+    //step-1:Allocate the memory
+    Arr=(int *)calloc(length , sizeof(int));
+
+    if(Arr==NULL)
+    {
+        printf("Unable to allocate memeory\n");
+    }
+    else
+    {
+        printf("Memory gets successfully allocated\n");
+    }
+
+    //step-2: Use the memory
+
+    //step-3:Free the memory
+    free(Arr);
+    
+
+    return 0;
+}
